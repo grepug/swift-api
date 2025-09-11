897c08c1e149142da086d5c3180c4e75aa7a66e2
Kai Shao <grepug@icloud.com>
2025-09-11 12:41:00 +0800

    Add upsertUserInfo method and related structures to UserEndpointGroupProtocol for enhanced user information management

--- Sources/Endpoints/User/UserEndpoint.swift
+++ Sources/Endpoints/User/UserEndpoint.swift
@@ -5,6 +5,7 @@
 //  Created by Kai Shao on 2025/4/17.
 //
 
+import Foundation
 import SwiftAPICore
 
 public enum EP {
@@ -14,13 +15,35 @@ public enum EP {
 
 public protocol UserEndpointGroupProtocol: EndpointGroupProtocol {
     typealias E1 = EP.User.FetchFreeFeature
+    typealias E2 = EP.User.UpsertUserInfo
 
     func fetchUserRequestFreeFeature(context: Context<E1>) async throws -> E1.Content
+    func upsertUserInfo(context: Context<E2>) async throws -> E2.Content
 }
 
 extension UserEndpointGroupProtocol {
     @RouteBuilder
     public var routes: Routes {
         Route().block(E1.self, fetchUserRequestFreeFeature)
+        Route().block(E2.self, upsertUserInfo)
+    }
+}
+
+extension EP.User {
+    @Endpoint("upsert-user-info", .POST)
+    public struct UpsertUserInfo {
+        public var body: Body
+
+        @DTO public struct Body {
+            public var deviceId: UUID
+            public var userName: String?
+            public var modelName: String?
+            public var marketingModelName: String?
+            public var deviceName: String?
+            public var appVersion: String?
+            public var appBuild: String?
+            public var osVersion: String?
+            public var osName: String?
+        }
     }
 }
