8af55ba02fa9ef27ebb52177e97ba089e0970a59
Kai Shao <grepug@icloud.com>
2025-11-03 10:39:57 +0800

    Add SetRedis and RedisFetch endpoints to SystemEndpointGroupProtocol for enhanced Redis management

--- Sources/Endpoints/System/SystemEndpointGroup.swift
+++ Sources/Endpoints/System/SystemEndpointGroup.swift
@@ -2,14 +2,20 @@ import SwiftAPICore
 
 public protocol SystemEndpointGroupProtocol: EndpointGroupProtocol {
     typealias E1 = EP.System.AppConfig
+    typealias E2 = EP.System.SetRedis
+    typealias E3 = EP.System.RedisFetch
 
     func fetchAppConfig(context: Context<E1>) async throws -> E1.Content
+    func setRedis(context: Context<E2>) async throws -> E2.Content
+    func fetchRedis(context: Context<E3>) async throws -> E3.Content
 }
 
 extension SystemEndpointGroupProtocol {
     @RouteBuilder
     public var routes: Routes {
         Route().block(E1.self, fetchAppConfig)
+        Route().block(E2.self, setRedis)
+        Route().block(E3.self, fetchRedis)
     }
 }
 
@@ -33,6 +39,32 @@ extension EP.System {
             public var forceUpdate: Bool
             public var appReviewMode: Bool
             public var isAppReviewing: Bool
+        }
+    }
+
+    @Endpoint("set-redis", .POST)
+    public struct SetRedis {
+        public var body: Body
+
+        @DTO
+        public struct Body {
+            public var key: String
+            public var value: String
+        }
+    }
+
+    @Endpoint("fetch-redis", .POST)
+    public struct RedisFetch {
+        public var body: Body
+
+        @DTO
+        public struct Body {
+            public var key: String
+        }
+
+        @DTO
+        public struct Content {
+            public var value: String?
         }
     }
 }
