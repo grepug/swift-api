f1a8faa3cdd717837f2f7dfa1177cbcbab3e16e8
Kai Shao <grepug@icloud.com>
2025-11-04 16:58:32 +0800

    Add SuggestedFulltexts endpoint to SystemEndpointGroupProtocol for enhanced content retrieval

--- Sources/Endpoints/System/SystemEndpointGroup.swift
+++ Sources/Endpoints/System/SystemEndpointGroup.swift
@@ -4,10 +4,12 @@ public protocol SystemEndpointGroupProtocol: EndpointGroupProtocol {
     typealias E1 = EP.System.AppConfig
     typealias E2 = EP.System.SetRedis
     typealias E3 = EP.System.RedisFetch
+    typealias E4 = EP.System.SuggestedFulltexts
 
     func fetchAppConfig(context: Context<E1>) async throws -> E1.Content
     func setRedis(context: Context<E2>) async throws -> E2.Content
     func fetchRedis(context: Context<E3>) async throws -> E3.Content
+    func fetchSuggestedFulltexts(context: Context<E4>) async throws -> E4.Content
 }
 
 extension SystemEndpointGroupProtocol {
@@ -16,6 +18,7 @@ extension SystemEndpointGroupProtocol {
         Route().block(E1.self, fetchAppConfig)
         Route().block(E2.self, setRedis)
         Route().block(E3.self, fetchRedis)
+        Route().block(E4.self, fetchSuggestedFulltexts)
     }
 }
 
@@ -65,6 +68,25 @@ extension EP.System {
         @DTO
         public struct Content {
             public var value: String?
+        }
+    }
+
+    @Endpoint("suggested-fulltexts", .GET)
+    public struct SuggestedFulltexts {
+
+        @DTO
+        public struct Content {
+            @DTO
+            public struct Item {
+                public let title: String
+                public let filePath: String
+                public let coverImagePath: String?
+                public let author: String?
+                public let source: String?
+                public let description: String?
+            }
+
+            public var items: [Item]
         }
     }
 }
