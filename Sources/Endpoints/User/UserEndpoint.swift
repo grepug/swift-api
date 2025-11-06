161c5202aa02755b2046dbb4e63e79adff3b8341
Kai Shao <grepug@icloud.com>
2025-11-06 16:09:03 +0800

    Add timezoneOffsetToUTC field to UpsertUserInfo request body for improved time zone handling

--- Sources/Endpoints/User/UserEndpoint.swift
+++ Sources/Endpoints/User/UserEndpoint.swift
@@ -49,6 +49,7 @@ extension EP.User {
             public var osVersion: String?
             public var osName: String?
             public var apnsDeviceToken: String?
+            public var timezoneOffsetToUTC: Int?  // in minutes
         }
     }
 
